e56d717e90ec95c9316ae0aea4b58eb97d82908f
Dmitry Korotchuk <dmitrykorotchuk@mail.ru>
2023-02-03 03:52:18 +0300

    Project9. Find max with local function. V2

--- Project9_UsingFunctions/Program.cs
+++ Project9_UsingFunctions/Program.cs
@@ -20,7 +20,7 @@ int Max(int arg1, int arg2, int arg3) // Создаем новую функци�
 int max1 = Max(a1, a2, a3); // применяем функцию для чисел из кода
 int max2 = Max(a4, a5, a6);
 int max3 = Max(a7, a8, a9);
-int answ = Max(max1, max2, max3);
+int answ = Max(Max(a1, a2, a3), Max(a4, a5, a6), Max(a7, a8, a9));
 
 Console.WriteLine(answ);
 
