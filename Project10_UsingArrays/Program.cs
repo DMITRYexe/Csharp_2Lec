4739cef8f6bb601f13ba44a4c9b5e6b1f50c9c8d
Dmitry Korotchuk <dmitrykorotchuk@mail.ru>
2023-02-03 04:09:01 +0300

    Project2,. Find max with local function and array. V1

--- Project10_UsingArrays/Program.cs
+++ Project10_UsingArrays/Program.cs
@@ -1,16 +1,19 @@
 ﻿// 9. Нахождение максимального числа из 9 с использованием функций и массивов.
 Console.WriteLine("Привет! Эта программа ищет максимальное число из 9.");
 Console.WriteLine("Числа вводятся внутри кода. Программа использует функции и массивы.");
-int[] array = {1,12,32,43,54,65,76,87,98}; // вместо нескольких переменных int задали 1 массив
+int[] array = { 1, 12, 32, 43, 54, 65, 76, 87, 98 }; // вместо нескольких переменных int задали 1 массив
 
-int Max(int arg1, int arg2, int arg3) // Создаем новую функцию (она же подпрограмма) для нахождения max из 3
-{ // создаем тело функции
+array[0] = 100; // так можно менять числа массива
+
+int Max(int arg1, int arg2, int arg3)
+{
   int result = arg1;
   if (arg2 > result) result = arg2;
   if (arg3 > result) result = arg3;
-  return result; // возвращаем в качестве ответа переменную "result".
+  return result;
 }
 
-int answ = Max(Max(a1, a2, a3), Max(a4, a5, a6), Max(a7, a8, a9));
+int answ = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]),
+            Max(array[6], array[7], array[8])); // строки можно переносить. Логическое замыкание происходит только после ";"
 
 Console.WriteLine(answ);
